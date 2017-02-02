efd23548bf10f43dcb7b9cd4380cf31b55aa7da2
mark <mark@ipp.ph>
2017-02-02 17:51:18 +0800

    update template

--- modules/articles/client/controllers/articles.client.controller.js
+++ modules/articles/client/controllers/articles.client.controller.js
@@ -55,6 +55,7 @@ angular.module('articles').controller('ArticlesController', ['$scope', '$statePa
       $scope.error = null;
 
       if (!isValid) {
+        console.log($scope.article);
         $scope.$broadcast('show-errors-check-validity', 'articleForm');
 
         return false;
@@ -79,6 +80,33 @@ angular.module('articles').controller('ArticlesController', ['$scope', '$statePa
       $scope.article = Articles.get({
         articleId: $stateParams.articleId
       });
+      $scope.article.$promise.then(function(val) {
+        // console.log(nl2br(val.content));
+        $scope.article.content = nl2br(val.content);
+      });
     };
+
+    // display line breaks
+    function nl2br (str, is_xhtml) {
+      // http://kevin.vanzonneveld.net
+      // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+      // +   improved by: Philip Peterson
+      // +   improved by: Onno Marsman
+      // +   improved by: Atli Þór
+      // +   bugfixed by: Onno Marsman
+      // +      input by: Brett Zamir (http://brett-zamir.me)
+      // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+      // +   improved by: Brett Zamir (http://brett-zamir.me)
+      // +   improved by: Maximusya
+      // *     example 1: nl2br('Kevin\nvan\nZonneveld');
+      // *     returns 1: 'Kevin<br />\nvan<br />\nZonneveld'
+      // *     example 2: nl2br("\nOne\nTwo\n\nThree\n", false);
+      // *     returns 2: '<br>\nOne<br>\nTwo<br>\n<br>\nThree<br>\n'
+      // *     example 3: nl2br("\nOne\nTwo\n\nThree\n", true);
+      // *     returns 3: '<br />\nOne<br />\nTwo<br />\n<br />\nThree<br />\n'
+      var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' : '<br>'; // Adjust comment to avoid issue on phpjs.org display
+
+      return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+    }
   }
 ]);
